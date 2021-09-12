378d37e5f1b83e9d6b127f050baef96023e6ddd4
Muhammad Ali <m.alikhan555@gmail.com>
2021-09-13 02:35:15 +0500

    Chat With Group

--- ChatAppWithSignalR/Hubs/MessageHub.cs
+++ ChatAppWithSignalR/Hubs/MessageHub.cs
@@ -23,6 +23,16 @@ namespace ChatAppWithSignalR.Hubs
             return Clients.Client(connectionId).SendAsync("ReceiveMessage", message);
         }
 
+        public Task JoinGroup(string groupName)
+        {
+            return Groups.AddToGroupAsync(Context.ConnectionId, groupName);
+        }
+
+        public Task SendMessageToGroup(string groupName, string message)
+        {
+            return Clients.Group(groupName).SendAsync("ReceiveMessage", message);
+        }
+
         public override async Task OnConnectedAsync()
         {
             await Clients.All.SendAsync("UserConnected", Context.ConnectionId);
