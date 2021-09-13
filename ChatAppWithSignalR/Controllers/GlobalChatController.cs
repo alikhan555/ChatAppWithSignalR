c06094b9d3cabd741260fc058aa8659e9e17f920
MuhammadAli <m.alikhan555@gmail.com>
2021-09-13 17:47:14 +0500

    Send messages through controller.

--- ChatAppWithSignalR/Controllers/GlobalChatController.cs
+++ ChatAppWithSignalR/Controllers/GlobalChatController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ChatAppWithSignalR.Hubs;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,8 +10,28 @@ namespace ChatAppWithSignalR.Controllers
 {
     public class GlobalChatController : Controller
     {
+        private readonly IHubContext<MessageHub> _messageHubContext;
+
+        public GlobalChatController(IHubContext<MessageHub> messageHubContext)
+        {
+            _messageHubContext = messageHubContext;
+        }
+
         public IActionResult Index()
         {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Announcement()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Announcement(string message)
+        {
+            _messageHubContext.Clients.All.SendAsync("ReceiveMessage", message);
             return View();
         }
     }
